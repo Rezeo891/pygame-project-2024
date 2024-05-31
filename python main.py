3ff992b2cd5e1a0de78c90ec1096a90b0b60ce45
Lucas Zhang <lucasz89261@gmail.com>
2024-05-31 09:11:37 -0700

    update

---
+++ python main.py
@@ -0,0 +1,15 @@
+# Pygame Project
+# Lucas Zhang
+
+import random
+import pygame as pg
+
+# --CONSTANTS--
+# COLOURS
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+EMERALD = (21, 219, 147)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
